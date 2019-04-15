d46870dfa0bd34b9b58d9ebc4c59e14de1b81091
kanak Shrivastava <kanak.shri07@gmail.com>
2019-04-15 13:05:14 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -30,6 +30,7 @@ async def download_file(url, dest):
 
 async def setup_learner():
     await download_file(export_file_url, path/export_file_name)
+    await download_file(export_file_url2, path/export_file_name2)
     try:
         learn = load_learner(path, export_file_name)
         return learn
